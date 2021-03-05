340806e826b3cf64c5bbc2502518535f47b68f3a
beckerm <markabecker@gmail.com>
2021-03-04 16:40:19 -0800

    added

---
+++ archive_files.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+ 
+import argparse
+import sys, errno
+import gzip
+import datetime
+import os
+import time
+import re
+
+ 
+
+parser = argparse.ArgumentParser(description='Archive and move files (using gzip).')
+
+
+parser.add_argument('-s', '--source', help='Source directory.', required=True)
+parser.add_argument('-a', '--archive', help='Archive directory.', required=True)
+parser.add_argument('-d', '--days', help='Days old.', type=int, required=True)
+parser.add_argument('-r', '--regex', help='Regular expression.  Default = ".*"', default='.*')
+
+args = parser.parse_args()
+
+
+file_regex = re.compile(args.regex)
+ 
+now   = time.time()
+days_old = int(args.days)
+
+
+def gzip_cost_file(file_to_zip):
+
+    zipped_file = file_to_zip + '.gz'
+
+    try:
+
+        fp = open(file_to_zip,"rb")
+
+        data = fp.read()
+
+        bindata = bytearray(data)
+
+        with gzip.open(zipped_file, "wb") as f:
+
+            f.write(bindata)
+
+    except IOError as err:
+
+        print('Error: ', err)
+
+        pass
+
+    finally:
+
+        if os.path.exists(file_to_zip):
+
+            try:
+
+                os.remove(file_to_zip)
+
+            except PermissionError as err:
+
+                print('Error: ', err)
+
+                pass
+
+        return zipped_file
+
+ 
+
+ 
+
+if os.path.exists(args.source):
+
+ 
+
+    if os.path.exists(args.archive):
+
+ 
+
+        for f in os.listdir(args.source):
+
+ 
+
+            full_path = os.path.join(args.source,f)
+
+ 
+
+            if os.path.isfile(full_path):
+
+ 
+
+                if file_regex.search(f):
+
+ 
+
+                    if os.stat(full_path).st_mtime < now - (days_old * 86400):
+
+                        full_path_source_archived_file=gzip_cost_file(full_path)
+
+                        destination_archived_file = os.path.join(full_path_source_archived_file.split('/')[-1])
+
+                        full_path_destination_archived_file = os.path.join(args.archive, destination_archived_file)
+
+                        os.rename(full_path_source_archived_file, full_path_destination_archived_file)
+
+   
+
+    else:
+
+        print('Invalid archive directory: ' + args.archive)
+
+        exit(1)
+
+ 
+
+ 
+
+else:
+
+    print('Invalid source directory: ' + args.source)
+
+    exit(1)
