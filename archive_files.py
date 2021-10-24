47e8bfb9aff9736932cf20540bac5a3c1f576106
beckerm <markabecker@gmail.com>
2021-10-24 13:05:43 -0700

    exception tweak

--- archive_files.py
+++ archive_files.py
@@ -1,69 +1,72 @@
 #!/usr/bin/env python3
 
- 
+
 import argparse
-import sys, errno
 import gzip
-import datetime
 import os
 import time
 import re
 import shutil
 
 
-parser = argparse.ArgumentParser(description='Archive and move files (using gzip).')
+parser = argparse.ArgumentParser(
+    description='Archive and move files (using gzip).')
 
 parser.add_argument('-s', '--source', help='Source directory.', required=True)
-parser.add_argument('-a', '--archive', help='Archive directory.', required=True)
+parser.add_argument('-a', '--archive',
+                    help='Archive directory.', required=True)
 parser.add_argument('-d', '--days', help='Days old.', type=int, required=True)
-parser.add_argument('-r', '--regex', help='Regular expression.  Default = ".*"', default='.*')
+parser.add_argument(
+    '-r', '--regex', help='Regular expression.  Default = ".*"', default='.*')
 
 args = parser.parse_args()
 
 file_regex = re.compile(args.regex)
- 
-now   = time.time()
+
+now = time.time()
 days_old = int(args.days)
 
 
 def gzip_file(file_to_zip):
     zipped_file = file_to_zip + '.gz'
     try:
-        with open(file_to_zip, 'rb') as f_in:   
+        with open(file_to_zip, 'rb') as f_in:
             with gzip.open(zipped_file, 'wb') as f_out:
                 f_out.writelines(f_in)
     except IOError as err:
         print('Error: ', err)
         pass
-    finally:
+    else:
         if os.path.exists(file_to_zip):
             try:
                 os.remove(file_to_zip)
-            except PermissionError as err:
+            except IOError as err:
                 print('Error: ', err)
                 pass
         return zipped_file
 
 
- 
 if os.path.exists(args.source):
 
     if os.path.exists(args.archive):
 
         for f in os.listdir(args.source):
-            full_path = os.path.join(args.source,f)
+            full_path = os.path.join(args.source, f)
 
             if os.path.isfile(full_path):
 
                 if file_regex.search(f):
 
                     if os.stat(full_path).st_mtime < now - (days_old * 86400):
-                        full_path_source_archived_file=gzip_file(full_path)
-                        destination_archived_file = os.path.join(full_path_source_archived_file.split('/')[-1])
-                        full_path_destination_archived_file = os.path.join(args.archive, destination_archived_file)
+                        full_path_source_archived_file = gzip_file(full_path)
+                        destination_archived_file = os.path.join(
+                            full_path_source_archived_file.split('/')[-1])
+                        full_path_destination_archived_file = os.path.join(
+                            args.archive, destination_archived_file)
                         try:
-                            shutil.move(full_path_source_archived_file, full_path_destination_archived_file)
-                        except PermissionError as err:
+                            shutil.move(full_path_source_archived_file,
+                                        full_path_destination_archived_file)
+                        except IOError as err:
                             print('Error: ', err)
                             pass
     else:
@@ -73,6 +76,3 @@ if os.path.exists(args.source):
 else:
     print('Invalid source directory: ', args.source)
     exit(1)
-
-
-
